6f2aff3a9df3a1acfddefee63b6bf65af59d4f05
Yuan Cheung <zhangyuan@users.noreply.github.com>
2016-07-13 13:06:38 +0800

    add html page and title

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
- module.exports = {
-     entry: './app/application.js',
-     output: {
-         path: './dist',
-         filename: 'application.bundle.js'
-     },
-     module: {
-         loaders: [{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader',
-         }]
-     }
- };
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/application.js',
+  output: {
+    path: './dist',
+    filename: 'application.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      title: "Simple Timer",      
+      filename: 'index.html', 
+      template: 'app/application.html'     
+      // hash: true
+    })
+  ],
+  module: {
+      loaders: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+      }]
+  }
+};
