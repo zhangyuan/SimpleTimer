278ed638a685f2c69f5d39d5968b8d5c791253a0
Yuan Cheung <zhangyuan@users.noreply.github.com>
2016-07-14 19:18:07 +0800

    add clock

--- app/application.js
+++ app/application.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var vsprintf = require("sprintf-js").vsprintf;
+
 require('normalize.css');
 class App extends React.Component {
   render() {
-    return <h1>Hello world!</h1>; 
+    return <Clock />
   }
 }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remainngTime: 68,
+    };
+  }
+
+  render() {
+    var remainngTime = this.state.remainngTime;
+    let minutes = parseInt(remainngTime / 60);
+    let seconds = remainngTime % 60;
+
+    return(
+      <div className="clock">
+        <span className="minutes">{vsprintf("%02d", [minutes])}</span>
+        <span className="separator">:</span>
+        <span className="seconds">{vsprintf("%02d", [seconds])}</span>
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('app')
