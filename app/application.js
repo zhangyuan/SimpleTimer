51c12b1010e0c41ae94211941ceb368bee211f3d
Yuan Cheung <zhangyuan@users.noreply.github.com>
2016-07-14 19:43:32 +0800

    simple timer with pre-defined remaining time

--- app/application.js
+++ app/application.js
@@ -1,12 +1,17 @@
+require('normalize.css');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 var vsprintf = require("sprintf-js").vsprintf;
 
-require('normalize.css');
 class App extends React.Component {
   render() {
-    return <Clock />
+    return <Clock remainingTime="3" onStop={this.onStop}/>
+  }
+
+  onStop() {
+    console.log("on stop");
   }
 }
 
@@ -14,14 +19,42 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      remainngTime: 68,
+      remainingTime: props.remainingTime,
+      onStop: props.onStop,
+      status: "off"
     };
   }
 
+  componentDidMount(){
+    this.kickoff();
+  }
+
+  kickoff() {
+    this.setState({status: "on"}); 
+    const interval = 10;    
+    this.tick(interval)   
+  }
+
+  tick(interval){    
+    var remainingTime = this.state.remainingTime - interval/1000.0;
+    if(remainingTime <= 0) {
+      this.stop();
+    } else {
+      this.setState({remainingTime: remainingTime});
+      setTimeout(() => {
+        this.tick(interval)
+      }, interval);
+    }
+  }
+
+  stop(){
+    this.state.onStop()
+  }
+
   render() {
-    var remainngTime = this.state.remainngTime;
-    let minutes = parseInt(remainngTime / 60);
-    let seconds = remainngTime % 60;
+    var remainingTime = parseInt(this.state.remainingTime);
+    let minutes = remainingTime / 60;
+    let seconds = remainingTime % 60;
 
     return(
       <div className="clock">
